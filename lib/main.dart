1c7ca4c570bd6461bc91fcfefd6d0d39b6847f43
Luis <luis.henrique8000@gmail.com>
2021-03-10 15:48:05 -0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:app_cursos_ui/constants.dart';
+import 'package:app_cursos_ui/details_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'models/category.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App de Cursos',
+      theme: ThemeData(),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                  builder: (context) => DetailsScreen(),
+    ),
+    );
+                  },
+                    child: SvgPicture.asset("icons/menu.svg")),
+                Image.asset("images/user.png"),
+
+              ],
+            ),
+            SizedBox(height: 30),
+            Text("Olá Alex,", style: kHeadingTextStyle),
+            Text("Encontre um curso que deseja aprender", style: kSubheadingTextStyle),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 30),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F5F7),
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Row(
+                children: <Widget>[
+                  SvgPicture.asset("icons/search.svg"),
+                  SizedBox(width: 16),
+                  Text(
+                    "Procure qualquer coisa",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xFFA0A5BD),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Categoria", style: kTitleTextStyle),
+                Text(
+                  "Veja todos",
+                  style: kSubtitleTextStyle.copyWith(color: kBlueColor),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: StaggeredGridView.countBuilder(
+                padding: EdgeInsets.all(0),
+                crossAxisCount: 2,
+                itemCount: categories.length,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.all(20),
+                    height: index.isEven ? 200 : 240,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      image: DecorationImage(
+                        image: AssetImage(categories[index].image),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          categories[index].name,
+                          style: kTitleTextStyle,
+                        ),
+                        Text(
+                          '${categories[index].numOfCourses} Cursos',
+                          style: TextStyle(
+                            color: kTextColor.withOpacity(.5),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+                staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
